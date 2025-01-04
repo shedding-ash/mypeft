1e6d720a11654946064061c4fb5a994f9181ce34
shedding_ash <shedding.ash@gmail.com>
2025-01-04 21:47:03 +0800

    修复错误引用

--- src/peft/tuners/Clora/layer.py
+++ src/peft/tuners/Clora/layer.py
@@ -24,12 +24,13 @@ from accelerate.utils.imports import is_xpu_available
 from torch import svd_lowrank
 from transformers.pytorch_utils import Conv1D
 
+from peft.tuners.lora.layer import LoraLayer
 from peft.tuners.tuners_utils import BaseTunerLayer, check_adapters_to_merge
 from peft.utils.integrations import dequantize_module_weight, gather_params_ctx, get_bnb_param_type
 from peft.utils.other import transpose
 
-from .config import LoraConfig,CLoraConfig
-from lora import LoraLayer
+from .config import LoraConfig
+
 # from .dora import DoraConv2dLayer, DoraConv3dLayer, DoraEmbeddingLayer, DoraLinearLayer, _DoraConvNdLayer
 
 
